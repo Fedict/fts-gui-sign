f088f1fc0dfaf52f5906563a8fc7bd73cfcbda0a
Laura De Groof <laura.degroof@zetes.com>
2020-04-28 13:30:07 +0200

    add browser detect + add link to extention

---
+++ src/modules/browserDetection/BrowserDetection.js
@@ -0,0 +1,42 @@
+export const browser = {
+    CHROME: "CHROME",
+    FIREFOX: "FIREFOX",
+    IE: "INTERNET EXPLORER",
+    EDGE: "EDGE",
+    SAFARI: "SAFARI",
+    OPERA: "OPERA",
+    OTHERS: "OTHERS"
+}
+
+export const getBrowser = () => {
+    // CHROME
+    if (navigator.userAgent.indexOf("Chrome") != -1) {
+        return (browser.CHROME)
+    }
+    // FIREFOX
+    else if (navigator.userAgent.indexOf("Firefox") != -1) {
+        return (browser.FIREFOX)
+    }
+    // INTERNET EXPLORER
+    else if (navigator.userAgent.indexOf("MSIE") != -1) {
+        return (browser.IE)
+
+    }
+    // EDGE
+    else if (navigator.userAgent.indexOf("Edge") != -1) {
+        return (browser.EDGE)
+
+    }
+    // SAFARI
+    else if (navigator.userAgent.indexOf("Safari") != -1) {
+        return (browser.SAFARI)
+    }
+    // OPERA
+    else if (navigator.userAgent.indexOf("Opera") != -1) {
+        return (browser.OPERA)
+    }
+    // OTHERS
+    else {
+        return(browser.OTHERS)
+    }
+};
--- src/modules/wizard/pages/VersionCheckInstallExtentionContainer.js
+++ src/modules/wizard/pages/VersionCheckInstallExtentionContainer.js
@@ -4,14 +4,29 @@ import { CardContainer } from '../../components/CardContainer/CardContainer'
 import { navigateToStep } from "../actions/WizardActions"
 import { WIZARD_STATE_VERSION_CHECK_LOADING } from '../wizard/WizardConstants'
 import { resetWizard } from '../actions/WizardLogicActions'
+import { isChrome, isEdgeChromium, getBrowser, browser } from '../../browserDetection/BrowserDetection'
 export class VersionCheckInstallExtentionContainer extends React.Component {
 
-    //TODO loop to check if correct version is installed
     handleButtonNextClick() {
         window.location.reload();
     }
 
-    getExtentionLink() {
+    openExtentionLink() {
+        let url
+        if (window.configData && window.configData.eIDLinkExtentionUrls) {
+            if ((getBrowser() === browser.CHROME) && window.configData.eIDLinkExtentionUrls.chrome) {
+                console.log("ik ben hier")
+                url = window.configData.eIDLinkExtentionUrls.chrome
+            }
+            if ((getBrowser() === browser.EDGE) && window.configData.eIDLinkExtentionUrls.edge) {
+                url = window.configData.eIDLinkExtentionUrls.edge
+            }
+
+        }
+
+        if (url) {
+            window.open(url, "_blank")
+        }
         //return correct link for browser
     }
     render() {
@@ -28,7 +43,7 @@ export class VersionCheckInstallExtentionContainer extends React.Component {
                     <p>Er is geen versie van eIDLink extentie gevonden.</p>
                     <p>Gelieve eIDlink te instaleren om gebruik te kunnen maken van deze applicatie</p>
 
-                    <button className="btn btn-primary" id="button_install_eID">
+                    <button className="btn btn-primary" id="button_install_eID" onClick={() => { this.openExtentionLink() }}>
                         Download en installeer eIDLink extentie
                         </button>
 
