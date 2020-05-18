7e47554df62771e4831b6df3ff7ed95c83437d34
Laura De Groof <laura.degroof@zetes.com>
2020-05-18 09:45:11 +0200

    change min version

--- public/config/config.js
+++ public/config/config.js
@@ -1,5 +1,5 @@
 window.configData = {
-    eIDLinkMinimumVersion: "1.0.0",
+    eIDLinkMinimumVersion: "1.0.1",
     eIDLinkExtensionUrls: {
         chrome: "https://chrome.google.com/webstore/detail/eazylink/pencgnkbgaekikmiahiaakjdgaibiipp",
         edge: "",
