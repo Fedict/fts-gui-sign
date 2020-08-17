80042da49f14f8b2213c0c5bee2c32a05a75fecd
Laura De Groof <laura.degroof@zetes.com>
2020-08-17 10:12:05 +0200

    add tests wizardlogicActions of validate

--- src/modules/validateWizard/actions/WizardLogicActions.test.js
+++ src/modules/validateWizard/actions/WizardLogicActions.test.js
@@ -1,13 +1,53 @@
+import { resetStore } from "../../../store/storeActions"
+import * as storeActions from "../../../store/storeActions";
+import { setNewFlowId } from "../../controlIds/flowId/FlowIdActions";
+import * as flowIdActions from "../../controlIds/flowId/FlowIdActions";
+import { navigateToStep } from "../../wizard/WizardActions";
+import * as wizardActions from "../../wizard/WizardActions";
+import { resetWizard } from "./WizardLogicActions";
+import { WIZARD_STATE_START } from "../../wizard/WizardConstants";
+
+
+const ORIGINAL_resetStore = resetStore
+const ORIGINAL_setNewFlowId = setNewFlowId
+const ORIGINAL_navigateToStep = navigateToStep
+
 describe("WizardLogicActions", () => {
     describe("resetWizard", () => {
-        test("resetWizard resets store", () => { })
-        test("resetWizard creates a new flowId", () => { })
-        test("resetWizard navigates to WIZARD_STATE_START", () => { })
-    })
 
-    describe("handleFlowIdError", () => {
-        test("handleFlowIdError returns response if flowID in the store is the same as the flowID in att", () => { })
-        test("handleFlowIdError throws error if flowID in the store is not the same as the flowID in att", () => { })
+        beforeEach(() => {
+            storeActions.resetStore = jest.fn()
+            flowIdActions.setNewFlowId = jest.fn()
+            wizardActions.navigateToStep = jest.fn()
+        })
+
+        afterEach(() => {
+            storeActions.resetStore = ORIGINAL_resetStore
+            flowIdActions.setNewFlowId = ORIGINAL_setNewFlowId
+            wizardActions.navigateToStep = ORIGINAL_navigateToStep
+        })
+
+        test("resetWizard resets store", () => {
+            const mockDispatch = jest.fn()
+            resetWizard()(mockDispatch)
+
+            expect(storeActions.resetStore).toBeCalledTimes(1)
+        })
+
+        test("resetWizard creates a new flowId", () => {
+            const mockDispatch = jest.fn()
+            resetWizard()(mockDispatch)
+
+            expect(flowIdActions.setNewFlowId).toBeCalledTimes(1)
+        })
+
+        test("resetWizard navigates to WIZARD_STATE_START", () => {
+            const mockDispatch = jest.fn()
+            resetWizard()(mockDispatch)
+
+            expect(wizardActions.navigateToStep).toBeCalledTimes(1)
+            expect(wizardActions.navigateToStep).toBeCalledWith(WIZARD_STATE_START)
+        })
     })
 
     describe("validateDocument", () => {
