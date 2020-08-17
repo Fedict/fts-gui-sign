d4c0d806469958246ccf5408956384d626e36dfe
Laura De Groof <laura.degroof@zetes.com>
2020-08-17 15:40:44 +0200

    remove is mobile block

--- src/modules/browserDetection/BrowserDetection.test.js
+++ src/modules/browserDetection/BrowserDetection.test.js
@@ -1,6 +1,7 @@
-import { getBrowser, browser } from "./BrowserDetection"
+import { getBrowser, browser,  browserIsAccepted } from "./BrowserDetection"
 
 const ORIGINAL_window = { ...window }
+
 describe("tests of BrowserDetection", () => {
 
     beforeAll(() => {
@@ -27,7 +28,7 @@ describe("tests of BrowserDetection", () => {
 
         expect(result).toEqual(browser.FIREFOX)
     })
-    
+
     test("detects safari based on navigator.userAgent string", () => {
         const navigatorUserAgentString = "Mozilla/5.0 (iPhone; CPU iPhone OS 11_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/11.0 Mobile/15E148 Safari/604.1 980x1306"
 
@@ -35,6 +36,45 @@ describe("tests of BrowserDetection", () => {
         const result = getBrowser()
 
         expect(result).toEqual(browser.SAFARI)
+    })
+
+    describe("browserIsAccepted", () => {
+
+        test("browserIsAccepted will return false if browser is mobile browser (Android Browser)", () => {
+            const navigatorUserAgentString = "Mozilla/5.0 (Linux; U; Android 4.4.2; en-us; SCH-I535 Build/KOT49H) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30"
+
+            window.navigator.userAgent = navigatorUserAgentString
+            const result = browserIsAccepted()
+
+            expect(result).toEqual(false)
+        })
+
+        test("browserIsAccepted will return false if browser is mobile browser (Firefox for Android)", () => {
+            const navigatorUserAgentString = "Mozilla/5.0 (Android 7.0; Mobile; rv:54.0) Gecko/54.0 Firefox/54.0"
+
+            window.navigator.userAgent = navigatorUserAgentString
+            const result = browserIsAccepted()
+
+            expect(result).toEqual(false)
+        })
+
+          test("browserIsAccepted will return true if browser is supported", () => {
+            const navigatorUserAgentString = "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:15.0) Gecko/20100101 Firefox/15.0.1"
+
+            window.navigator.userAgent = navigatorUserAgentString
+            const result = browserIsAccepted()
+
+            expect(result).toEqual(true)
+        }) 
+
+        test("browserIsAccepted will return false if browser not is supported", () => {
+            const navigatorUserAgentString = "doen not exists"
+
+            window.navigator.userAgent = navigatorUserAgentString
+            const result = browserIsAccepted()
+
+            expect(result).toEqual(false)
+        })
     })
 
     afterAll(() => {
