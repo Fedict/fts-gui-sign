cb22b2f8607b52746a87d2baa940c138c4af9f6e
Carl Bosch <carl.bosch@zetes.com>
2021-07-06 11:57:12 +0200

    Correct unit test

--- src/modules/signByTokenWizard/pages/TokenWizardIntroContainer.test.js
+++ src/modules/signByTokenWizard/pages/TokenWizardIntroContainer.test.js
@@ -5,12 +5,13 @@ import React from "react";
 describe("TokenWizardIntroContainer", () => {
     test("shows start button & can click on it",  done => {
         const getCertificates = jest.fn();
+        const doSendLogInfo = jest.fn();
 
         getCertificates.mockImplementation((callback) => {
             callback(true);
         });
 
-        render(<TokenWizardIntroComponent navigateToNextStep={done} getCertificates={getCertificates}/>);
+        render(<TokenWizardIntroComponent navigateToNextStep={done} getCertificates={getCertificates} doSendLogInfo={doSendLogInfo}/>);
 
         const buttonElement = screen.getByRole('button', {name : /SIGN/i});
 
@@ -24,6 +25,7 @@ describe("TokenWizardIntroContainer", () => {
 
         expect(documentReadCheckBox).toBeInTheDocument();
         documentReadCheckBox.click();
+        expect(doSendLogInfo).toBeCalled();
 
         expect(buttonElement).toBeEnabled();
         buttonElement.click();
