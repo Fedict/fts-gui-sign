c50abec8271e8664a664ff17176d065c4a3031f4
kennethVDL <kenneth.watashi@hotmail.com>
2021-12-06 16:26:18 +0100

    Changed start sign and valid pages. Fixed crash

--- src/modules/startPage/StartPageContainer.js
+++ src/modules/startPage/StartPageContainer.js
@@ -1,38 +1,60 @@
 import React from 'react'
+import { connect } from 'react-redux'
 import { withRouter } from 'react-router-dom'
 import { CardContainer } from '../components/Card/CardContainer'
 import { MethodeSelectCard } from '../components/MethodSelect/MethodSelectCard'
+import { resetWizard } from "../validateWizard/actions/WizardLogicActions";
+import DisplayFile from '../fileUpload/components/UploadDisplayFile/UploadDisplayFile'
+import { defineMessages, FormattedMessage, injectIntl } from "react-intl";
 
 export class StartPageContainer extends React.Component {
 
     render() {
         const { history } = this.props
+        const { resetWizard } = this.props
 
         return (
-            <div className={"container"}>
-                <CardContainer
-                    title="Select the method to sign"
-                >
-                    <MethodeSelectCard
-                        index={0}
-                        id={"MethodSelect"}
-                        name={"eID"}
-                        onClick={() => { history.push('/sign') }}
-                        imgSrc={"./img/SignIcons/personIcon.svg"} />
-                    <MethodeSelectCard
-                        index={1}
-                        id={"MethodSelect"}
-                        name={"something else"}
-                        url={"https://www.google.com/"}
-                        imgSrc={"./img/SignIcons/emailIcon.svg"} />
-                </CardContainer>
+            <div >
+                <div className={"row mx-5 mt-3"}>
+                    <div className={"col col-7"}>
+                        <   DisplayFile />
+                    </div>
+                    <div className={"col col-5"}>
+
+                        <div className={"container"}>
+                            <div class="card">
+                                <div class="card-header"><FormattedMessage id="index.title" defaultMessage="Signing Box" /></div>
+                                <div class="card-body">
+                                    <b><FormattedMessage id="index.welcome" defaultMessage="Welcome" /></b>
+                                    <br /><br />
+                                    <FormattedMessage id="index.content" defaultMessage="This application will allow you to electronically sign files and validate signed files, by means of your electronic identity card (eID). " />
+                                    <br />
+                                    <br />
+                                    <div class="row mx-5 mt-3">
+                                        <div class="col col-6">
+                                            <p align="center"><button style={{ minWidth: 115 }} className="btn btn-primary" id="button_install_eID" onClick={() => { resetWizard(); history.push('/sign') }}><FormattedMessage id="buttons.sign" defaultMessage="Sign" /></button></p>
+                                        </div>
+                                        <div class="col col-6">
+                                            <p align="center"><button style={{ minWidth: 115 }} className="btn btn-primary" id="button_install_eID" onClick={() => { resetWizard(); history.push('/validate') }}><FormattedMessage id="buttons.validate" defaultMessage="Validate" /></button></p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="card-footer">
+                                    <b><FormattedMessage id="index.footer" defaultMessage="If you want more information about eID cards and eID readers, go to " /><a href="https://eid.belgium.be/">EID.BELGIUM.BE</a></b>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
 }
 
+const mapDispatchToProps = ({
+    resetWizard
+})
 
 
 
-
-export default withRouter((StartPageContainer))
+export default withRouter(connect(null,mapDispatchToProps)(StartPageContainer))
