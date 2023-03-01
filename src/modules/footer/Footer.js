f326807cc3f172a224e355776cda80f2260f7df2
Christian <work@xylsys.com>
2023-03-01 10:57:44 +0100

    Fix: French accesibility statement missing

--- src/modules/footer/Footer.js
+++ src/modules/footer/Footer.js
@@ -16,7 +16,7 @@ export const Footer = (props) => {
                 </Link>  -  </>}
                 <Link to={'/ps' + httpParams}>
                     <FormattedMessage id="footer.genPrivacy" defaultMessage="Privacy Statement"/>
-                </Link>{ locale === "fr" || locale === "nl" && <> - <a href= { locale === "fr" ? "https://bosa.belgium.be/fr/declaration-daccessibilite" : "https://bosa.belgium.be/nl/toegankelijkheidsverklaring" }>
+                </Link>{ (locale === "fr" || locale === "nl") && <> - <a href= { locale === "fr" ? "https://bosa.belgium.be/fr/declaration-daccessibilite" : "https://bosa.belgium.be/nl/toegankelijkheidsverklaring" }>
                     <FormattedMessage id="footer.accStatement" defaultMessage="Accessibility statement"/>
                 </a></>}
             </div>
