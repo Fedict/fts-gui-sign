868776e1ffe44323fbcf7e2d2b9c3e0b5c671b8e
Christophe Peerens <c.peerens@neomytic.be>
2023-03-01 09:51:02 +0100

    Cosmetic on drag drop display

--- src/modules/components/UploadFile/UploadFileContainer.js
+++ src/modules/components/UploadFile/UploadFileContainer.js
@@ -5,8 +5,8 @@ import { connect } from 'react-redux';
 import { CardContainer } from '../../components/Card/CardContainer';
 import { NumberdText } from '../../components/NumberedText/NumberdText';
 import { WIZARD_STATE_CERTIFICATES_LOADING, WIZARD_STATE_VALIDATE_LOADING } from '../../wizard/WizardConstants';
-import {defineMessages, FormattedMessage, injectIntl} from "react-intl";
-import {boldedText} from "../../utils/reactIntlUtils";
+import { defineMessages, FormattedMessage, injectIntl } from "react-intl";
+import { boldedText } from "../../utils/reactIntlUtils";
 import { sendLogInfo } from "../../communication/communication"
 
 const messagesSigning = defineMessages({
@@ -31,8 +31,8 @@ const messagesValidate = defineMessages({
     }
 })
 
-  const styleDragGreen = {
-    backgroundColor : "#00FF0011",
+const styleDragGreen = {
+    backgroundColor: "#00FF0011",
     position: "absolute",
     width: "100%",
     height: "100%",
@@ -41,10 +41,10 @@ const messagesValidate = defineMessages({
     right: "0px",
     bottom: "0px",
     left: "0px",
-  };
+};
 
-  const styleDragRed = {
-    backgroundColor : "#FF000011",
+const styleDragRed = {
+    backgroundColor: "#FF000011",
     position: "absolute",
     width: "100%",
     height: "100%",
@@ -53,14 +53,14 @@ const messagesValidate = defineMessages({
     right: "0px",
     bottom: "0px",
     left: "0px",
-  };
+};
 
 export const UploadFileContainer = (props) => {
     const { intl, UploadFileContext } = props;
     const [file, setFile] = React.useState({});
     const [dragging, setDragging] = React.useState(false);
     const [draggingError, setDraggingError] = React.useState(false);
-    const messages = UploadFileContext==="sign"?messagesSigning:messagesValidate;
+    const messages = UploadFileContext === "sign" ? messagesSigning : messagesValidate;
 
     var handleDrop = function (e) {
         e.preventDefault();
@@ -72,7 +72,7 @@ export const UploadFileContainer = (props) => {
                 case 'xml':
                 case 'pdf':
                     setFile(e.dataTransfer.files[0]);
-                    if (UploadFileContext==="sign"){
+                    if (UploadFileContext === "sign") {
                         props.displayFile(e.dataTransfer.files[0])
                     }
                     return;
@@ -88,15 +88,15 @@ export const UploadFileContainer = (props) => {
         if (e.type === "dragenter" || e.type === "dragover") {
             setDragging(true);
             if (e.dataTransfer.items && e.dataTransfer.items.length === 1
-                && e.dataTransfer.items[0].kind==="file"
-                && (e.dataTransfer.items[0].type==="application/pdf" || e.dataTransfer.items[0].type==="application/xml" || e.dataTransfer.items[0].type==="text/xml")) {
+                && e.dataTransfer.items[0].kind === "file"
+                && (e.dataTransfer.items[0].type === "application/pdf" || e.dataTransfer.items[0].type === "application/xml" || e.dataTransfer.items[0].type === "text/xml")) {
                 setDraggingError(false);
                 return;
             }
-            else{
+            else {
                 setDraggingError(true);
             }
-          }
+        }
         else if (e.type === "dragleave") {
             setDragging(false);
             setDraggingError(false);
@@ -106,14 +106,14 @@ export const UploadFileContainer = (props) => {
     const onchange = (e) => {
         const fileSelected = e.target.files[0]
         setFile(fileSelected);
-        if (UploadFileContext==="sign"){
+        if (UploadFileContext === "sign") {
             props.displayFile(fileSelected)
         }
     }
 
     const handleSubmit = () => {
         props.uploadFile(file)
-        if (UploadFileContext==="sign"){
+        if (UploadFileContext === "sign") {
             props.navigateToStep(WIZARD_STATE_CERTIFICATES_LOADING)
         }
         else {
@@ -121,10 +121,7 @@ export const UploadFileContainer = (props) => {
         }
     }
 
-    console.log("file", file);
-    console.log("dragging", dragging);
-
-     return (
+    return (
         <CardContainer
             title={intl.formatMessage(messages.title)}
             hasNextButton
@@ -136,94 +133,104 @@ export const UploadFileContainer = (props) => {
                 <div className="container" >
                     <ol className="invisibleOL">
                         {
-                            UploadFileContext==="sign"
-                            ?
-                            <>
-                                <li><NumberdText number="1"><FormattedMessage id="signing.upload.text.step.1"
-                                                            defaultMessage="Select a document (pdf or xml) via the button <b>{selectDocumentButton}</b>."
-                                                            values={{b : boldedText, selectDocumentButton : intl.formatMessage({id : "signing.upload.selectDocument", defaultMessage : "Select a document"})}}
-                                                            /></NumberdText></li>
-                                <li><NumberdText number="2"><FormattedMessage id="signing.upload.text.step.2" defaultMessage="Connect your eID reader."/></NumberdText></li>
-                                <li><NumberdText number="3"><FormattedMessage id="signing.upload.text.step.3" defaultMessage="Insert your eID card in the card reader."/></NumberdText></li>
-                                <li><NumberdText number="4"><FormattedMessage id="signing.upload.text.step.4"
-                                                            defaultMessage="Click on the button <b>{signButton}</b> and enter your pincode when asked."
-                                                            values={{b : boldedText, signButton : intl.formatMessage({id : "signing.pininput.button.sign", defaultMessage : "Sign with eID"})}}
-                                                            /></NumberdText></li>
-                            </>
-                            :
-                            <>
-                                <li><NumberdText number="1"><FormattedMessage id="validate.upload.text.step.1"
-                                defaultMessage="Select the digitally signed document (pdf or xml) by clicking on the button <b>{selectDocumentButton}</b>."
-                                values={{b : boldedText, selectDocumentButton : intl.formatMessage({id : "signing.upload.selectDocument", defaultMessage : "Select a document"})}}
-                                /></NumberdText></li>
-                                <li><NumberdText number="2"><FormattedMessage id="validate.upload.text.step.2"
-                                defaultMessage="Click on the button '<b>{validateButton}</b>' to start the validation."
-                                values={{b : boldedText, validateButton : intl.formatMessage({id : "validate.upload.validateButton", defaultMessage : "Validate"})}}
-                                /></NumberdText></li>
-                            </>
-                            }
+                            UploadFileContext === "sign"
+                                ?
+                                <>
+                                    <li><NumberdText number="1"><FormattedMessage id="signing.upload.text.step.1"
+                                        defaultMessage="Select a document (pdf or xml) via the button <b>{selectDocumentButton}</b>, or Drag & Drop the document."
+                                        values={{ b: boldedText, selectDocumentButton: intl.formatMessage({ id: "signing.upload.selectDocument", defaultMessage: "Select a document" }) }}
+                                    /></NumberdText></li>
+                                    <li><NumberdText number="2"><FormattedMessage id="signing.upload.text.step.2" defaultMessage="Connect your eID reader." /></NumberdText></li>
+                                    <li><NumberdText number="3"><FormattedMessage id="signing.upload.text.step.3" defaultMessage="Insert your eID card in the card reader." /></NumberdText></li>
+                                    <li><NumberdText number="4"><FormattedMessage id="signing.upload.text.step.4"
+                                        defaultMessage="Click on the button <b>{signButton}</b> and enter your pincode when asked."
+                                        values={{ b: boldedText, signButton: intl.formatMessage({ id: "signing.pininput.button.sign", defaultMessage: "Sign with eID" }) }}
+                                    /></NumberdText></li>
+                                </>
+                                :
+                                <>
+                                    <li><NumberdText number="1"><FormattedMessage id="validate.upload.text.step.1"
+                                        defaultMessage="Select the digitally signed document (pdf or xml) by clicking on the button <b>{selectDocumentButton}</b>, or Drag & Drop the document."
+                                        values={{ b: boldedText, selectDocumentButton: intl.formatMessage({ id: "signing.upload.selectDocument", defaultMessage: "Select a document" }) }}
+                                    /></NumberdText></li>
+                                    <li><NumberdText number="2"><FormattedMessage id="validate.upload.text.step.2"
+                                        defaultMessage="Click on the button '<b>{validateButton}</b>' to start the validation."
+                                        values={{ b: boldedText, validateButton: intl.formatMessage({ id: "validate.upload.validateButton", defaultMessage: "Validate" }) }}
+                                    /></NumberdText></li>
+                                </>
+                        }
                     </ol>
-                    <div className="row" onDragEnter={handleDrag} onDrop={handleDrop}>
-                        <div className="card col col-12">
+                    <div className="row" onDragEnter={handleDrag} onDrop={handleDrop} style={{ border: "dashed 2px rgba(0, 0, 0, 0.125)", borderRadius: "0.25rem" }}>
+                        <div className="card col col-12" style={{ border: "none" }}>
                             <div className="card-body ">
-                                <div className="row ">
-                                    <div className="col col-auto align-self-center ">
-                                        <button
-                                            className='btn btn-primary'
-                                            type="button"
-                                            id="button_select_file"
-                                            value=""
-                                            onClick={() => {
-                                                document.getElementById('input_hidden_select_file').click()
-                                            }}
-                                        > <FormattedMessage id="signing.upload.selectDocument" defaultMessage="Select a document"/> </button>
-
-                                        <input
-                                            type="file"
-                                            accept=".pdf,.xml"
-                                            style={{ display: "none" }}
-                                            id="input_hidden_select_file"
-                                            onChange={(e) => { 
-                                                var ext = e.target.value.match(/.([^.]+)$/)[1];
-                                                switch (ext) {
-                                                    case 'xml':
-                                                    case 'pdf':
-                                                        if (UploadFileContext==="sign"){
-                                                            sendLogInfo('UI - Upload file to Sign');
-                                                        }
-                                                        else{
-                                                            sendLogInfo('UI - Upload file to Validate');
-                                                        }
-                                                        onchange(e)
-                                                        break;
-                                                    default:
-                                                        break;
+                                <div className="row " style={{ justifyContent: "center" }}>
+                                    <button
+                                        className='btn btn-primary'
+                                        type="button"
+                                        id="button_select_file"
+                                        value=""
+                                        onClick={() => {
+                                            document.getElementById('input_hidden_select_file').click()
+                                        }}
+                                    > <FormattedMessage id="signing.upload.selectDocument" defaultMessage="Select a document" /> </button>
+                                    <input
+                                        type="file"
+                                        accept=".pdf,.xml"
+                                        style={{ display: "none" }}
+                                        id="input_hidden_select_file"
+                                        onChange={(e) => {
+                                            var ext = e.target.value.match(/.([^.]+)$/)[1];
+                                            switch (ext) {
+                                                case 'xml':
+                                                case 'pdf':
+                                                    if (UploadFileContext === "sign") {
+                                                        sendLogInfo('UI - Upload file to Sign');
                                                     }
-                                            }} />
+                                                    else {
+                                                        sendLogInfo('UI - Upload file to Validate');
+                                                    }
+                                                    onchange(e)
+                                                    break;
+                                                default:
+                                                    break;
+                                            }
+                                        }} />
+                                </div>
+                                <div className="row " style={{ justifyContent: "center" }}>
+                                    <div className="col col-auto align-self-center ">
+                                        <p className="btn m-0" >
+                                            <FormattedMessage id="drag.drop.here" defaultMessage="Or Drag & Drop it here" />
+                                            <span id='name_select_file'></span>
+                                        </p>
                                     </div>
+                                </div>
+                                <div className="row " style={{ justifyContent: "center" }}>
                                     <div className="col col-auto align-self-center ">
                                         <p className="btn m-0" >
                                             <i>
-                                        <FormattedMessage id="signing.upload.selectedDocument" defaultMessage="Selected document:"/>
-                                                <span id='name_select_file'> {(file && file.name) || <FormattedMessage id="signing.upload.noDocumentSelected" defaultMessage="no document selected yet"/>}</span>
-                                                </i>
+                                                <FormattedMessage id="signing.upload.selectedDocument" defaultMessage="Selected document:" />
+                                                <span id='name_select_file'> {(file && file.name) || <FormattedMessage id="signing.upload.noDocumentSelected" defaultMessage="no document selected yet" />}</span>
+                                            </i>
                                         </p>
-                                        {draggingError && 
-                                        <p className="btn m-0" >
-                                            <span>Warning: Only drop 1 file of type PDF or XML</span>
-                                        </p>
-                                        }
                                     </div>
-                                    { dragging && <div onDragEnter={handleDrag} onDragLeave={handleDrag} onDragOver={handleDrag} onDrop={handleDrop} style={draggingError?styleDragRed:styleDragGreen}></div> }
                                 </div>
+                                {draggingError &&
+                                    <div className="row " style={{ justifyContent: "center" }}>
+                                        <div className="col col-auto align-self-center ">
+                                            <p className="btn m-0" style={{ backgroundColor: "#FF000011" }} >
+                                                <FormattedMessage id="drag.drop.error" defaultMessage="Only dropping one document of type PDF or XML is allowed!" />
+                                            </p>
+                                        </div>
+                                    </div>
+                                }
+                                {dragging && <div onDragEnter={handleDrag} onDragLeave={handleDrag} onDragOver={handleDrag} onDrop={handleDrop} style={draggingError ? styleDragRed : styleDragGreen}></div>}
                             </div>
                         </div>
                     </div>
-
                 </div>
             </div>
         </CardContainer>
-        )
+    )
 }
 
 
