2c40c374945c96c2a219db6dd67f592151428cc3
chmo <chmo>
2024-01-11 14:01:12 +0100

    Update CSP for local

--- public/config/config.js
+++ public/config/config.js
@@ -63,14 +63,17 @@ window.configData = {
 
     mxaxUploadSize: 10*1024*1024,
 
-    BEurl: "https://validate.ta.fts.bosa.belgium.be/signandvalidation",
-    //BEurl: "http://localhost:8751",
+    //BEurl: "https://validate.ta.fts.bosa.belgium.be/signandvalidation",
+    BEurl: "http://localhost:8751",
     
     // Local settings
-    //CSP: "default-src 'self'; connect-src 'self' http://localhost:8751  http://localhost:8081; script-src 'self' 'unsafe-inline'; style-src 'unsafe-inline'; object-src 'self' blob: data: http://localhost:8751; frame-src 'self' blob: data: http://localhost:8751;",
+    CSP: "default-src 'self'; connect-src blob: 'self' http://localhost:8751  http://localhost:8081 http://localhost:3000; script-src 'self' 'unsafe-inline'; style-src 'unsafe-inline' http://localhost:3000; object-src 'self' blob: data: http://localhost:8751; frame-src 'self' blob: data: http://localhost:8751;",
+
+    // Local settings PDF.js display
+    //CSP: "default-src 'self'; connect-src 'self' http://localhost:8751  http://localhost:8081; script-src 'self' 'unsafe-inline'; style-src 'unsafe-inline' ; object-src 'self' blob: data: http://localhost:8751; frame-src 'self' blob: data: http://localhost:8751;",
 
     // "TA" settings
-    CSP: "default-src 'self'; connect-src 'self' https://*.ta.fts.bosa.belgium.be; script-src 'self' 'unsafe-inline'; style-src 'unsafe-inline' https://*.ta.fts.bosa.belgium.be; object-src 'self' blob: data: https://*.ta.fts.bosa.belgium.be; frame-src 'self' blob: data: https://*.ta.fts.bosa.belgium.be;",
+    //CSP: "default-src 'self'; connect-src 'self' https://*.ta.fts.bosa.belgium.be; script-src 'self' 'unsafe-inline'; style-src 'unsafe-inline' https://*.ta.fts.bosa.belgium.be; object-src 'self' blob: data: https://*.ta.fts.bosa.belgium.be; frame-src 'self' blob: data: https://*.ta.fts.bosa.belgium.be;",
 
 
 
