36c944acecbb900b39b54a8044c156291ea564f0
chmo <chmo>
2024-05-07 16:50:16 +0200

    New date format for visible signature

--- src/modules/communication/communication.js
+++ src/modules/communication/communication.js
@@ -75,8 +75,8 @@ export const createBody = (certificateBody, documentName, documentBase64, docume
                 signLanguage,
                 "psp": sigType === INVISIBLE_SIGNATURE ? null : {
                     "texts": {
-                        "fr": "Signé par %gn% %sn%\nLe %d(d MMM YYYY z à HH:mm)%",
-                        "en": "Signed by %gn% %sn%\nOn %d(d MMM YYYY z on HH:mm )%",
+                        "fr": "Signé par %gn% %sn%\nLe %d(d MMM YYYY z - HH:mm)%",
+                        "en": "Signed by %gn% %sn%\nOn %d(d MMM YYYY z - HH:mm )%",
                         "de": "Unterzeichnet von %gn% %sn%\nAm %d(HH:mm MMM d YYYY z)%",
                         "nl": "Getekend door %gn% %sn%\nOp %d(HH:mm MMM d YYYY z)%",
                     },
